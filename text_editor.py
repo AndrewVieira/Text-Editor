757b5979ff6cc597d3522d791d4fba26311b1bc9
Andrew Vieira <avieira.contact@gmail.com>
2015-11-05 20:41:50 -0600

    Tabs!
    Added tabs so the user can work on multiple files with only one instance of the application open. Also changed many of the menu functions with this change. The save function does not work for now. Undo and redo have been added and function properly. Help has been added but currently has no function.

--- text_editor.py
+++ text_editor.py
@@ -4,75 +4,141 @@ from tkinter import ttk
 import file_manager as fm
 
 class TextEditorApp(tk.Frame):
-    def __init__ (self, master=None):
+    def __init__ (self, *args, **kwargs):
         #Outer Frame of the application
-        self.master = master
-        tk.Frame.__init__(self, self.master)
+        tk.Frame.__init__(self, *args, **kwargs)
         self.grid(sticky="nsew")
 
         #Name of the application at the top
         self.master.title("Text Editor")
 
+        #Window Size Stuff
+        self.master.geometry("640x480")
+        self.master.minsize(320, 240)
+
+        #Keep track of tabs
+        #The reason this is here is because notebook does not return
+        #child values of the tabs, so we have to keep track of it
+        #ourselves
+        self.tabs = {}
+
         #Create Widgets
         self.createWidgets()
-
-        #Other Variables
-        #Current File keeps track of which file the user is currently editing
-        self.current_file = None
 
     def createWidgets(self):
         #Set up top space
         top=self.winfo_toplevel()
         top.rowconfigure(0, weight=1)
         top.columnconfigure(0, weight=1)
-        
-        #The text box to edit code
-        self.textBox = tk.Text(self)
-        self.textBox.pack(side="bottom", fill= "both", expand=True)
 
+        #The Notebook so user can quickly shift between files
+        self.notebook = ttk.Notebook(self)
+        self.notebook.pack(side="top", fill = "both", expand=True)
+        
         #Menu Buttons
         self.menuBar = tk.Menu(top)
         top['menu'] = self.menuBar
-        self.fileMenu = tk.Menu(self.menuBar)
 
         #File
+        self.fileMenu = tk.Menu(self.menuBar)
         self.menuBar.add_cascade(label='File', menu=self.fileMenu)
+        self.fileMenu.add_command(label='New', command=self.newFile)
         self.fileMenu.add_command(label='Open', command=self.openFile)
+        self.fileMenu.add_separator()
         self.fileMenu.add_command(label='Save', command=self.saveFile)
         self.fileMenu.add_command(label='Save As', command=self.saveAsFile)
+        self.fileMenu.add_separator()
         self.fileMenu.add_command(label='Exit', command=quit)
+
+        #Edit
+        self.editMenu = tk.Menu(self.menuBar)
+        self.menuBar.add_cascade(label='Edit', menu=self.editMenu)
+        self.editMenu.add_command(label='Undo', command=self.undo)
+        self.editMenu.add_command(label='Redo', command=self.redo)
+
+        #About
+        self.helpMenu = tk.Menu(self.menuBar)
+        self.menuBar.add_cascade(label='Help', menu=self.helpMenu)
+        self.helpMenu.add_command(label='About', command=None)
+
+    #Find the value of the current tab textbox
+    def getTabText(self):
+        tabname = self.notebook.select()
+        return self.tabs[tabname]
+
+    #Creates a new textbox to edit files in
+    def newTextBox(self):
+        textbox = tk.Text(self, undo=True, autoseparators=True)
+        textbox.pack(side="bottom", fill= "both", expand=True)
+
+        self.tabs[str(textbox)] = textbox
+
+        return textbox
+
+    #Creates a new tab with a textbox
+    def newFile(self, name="Untitled"):
+        #Create new textbox
+        newTextBox = self.newTextBox()
+        
+        #Add textbox to tabs
+        self.notebook.add(newTextBox, text=name)
+
+        #Focus on new tab with new textbox
+        self.notebook.select(newTextBox)
 
     def openFile(self):
         #Get the filename and load the text
         filename = tk.filedialog.askopenfilename()
         loaded_text = fm.load_file(filename)
 
-        #Insert the text into the text box
-        self.textBox.delete('1.0', tk.END)
-        self.textBox.insert('1.0', loaded_text)
+        #Open up a new tab to load the file in
+        self.newFile()
 
-        #Change the current file and add the file name to the title
-        self.current_file = filename
-        self.master.title("Text Editor - " + self.current_file)
+        #Insert the text into the current text box
+        textbox = self.getTabText()
+        textbox.delete('1.0', tk.END)
+        textbox.insert('1.0', loaded_text)
+
+        #Add the file name to the title of the tab
+        self.notebook.tab(textbox, text=filename)
 
     def saveFile(self):
+        pass
+        """
+        #Get the current textbox
+        textbox = self.getTabText()
+
+        #Get current filename
+        current_file = 
+        
         #Save the file without creating a new file if we know it already exists
-        if self.current_file != None:
-            saved_text = self.textBox.get('1.0', tk.END)
-            fm.save_file(self.current_file, saved_text)
+        if current_file != None:
+            saved_text = textbox.get('1.0', tk.END)
+            fm.save_file(current_file, saved_text)
         else:
-            self.saveAsFile()
+            self.saveAsFile()"""
 
     def saveAsFile(self):
-        #Save the file by creating a new file
+        #Get the current textbox
+        textbox = self.getTabText()
+        
+        #Save the file by creating a new fill        
         filename = tk.filedialog.asksaveasfilename()
-        saved_text = self.textBox.get('1.0', tk.END)
+        saved_text = textbox.get('1.0', tk.END)
 
         fm.save_file(filename, saved_text)
 
-        #Change the current file and add the file name to the title
-        self.current_file = filename
-        self.master.title("Text Editor - " + self.current_file)
+        #Add the file name to the title of the tab
+        self.notebook.tab(textbox, text=filename)
+        #self.master.title("Text Editor - " + self.current_file)
+
+    #Undo text edit in current textbox
+    def undo(self):
+        self.getTabText().edit_undo()
+
+    #Redo text edit in current textbox
+    def redo(self):
+        self.getTabText().edit_redo()
 
 #Run the Program
 if __name__ == "__main__":
@@ -82,3 +148,4 @@ if __name__ == "__main__":
 
     #Run the Application
     app.mainloop()
+
